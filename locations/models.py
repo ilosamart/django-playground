dddf483a034c4c4d9cf133d478b7f41b726ef404
Fábio Tramasoli <fabio@tramasoli.com>
2017-10-20 18:31:42 -0200

    DONE - locations.models

---
+++ locations/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Country(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50, blank=False)
+
+
+class State(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50, blank=False)
+    country = models.ForeignKey(to=Country, related_name='states')
+
+
+class City(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50, blank=False)
+    pib = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    state = models.ForeignKey(to=State, related_name='cities')
